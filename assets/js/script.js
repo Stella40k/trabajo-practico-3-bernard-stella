b99e96d1298240c13f2d2851c31fed10790261d8
Estudiante <luquiscardenas@gmail.com>
2025-05-31 19:15:10 -0300

    se saco el boton de mostrar todos (pq sera por defeco), se agrego un margin entre las card y el navbar, se corrigio el nav por un navbar y se agrego la verificacion de data de personaje segun la id en js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
-const API_URL_BASE = "https://dragonball-api.com/api/characters"; //el url para las peticiones
+const API_URL_BASE = "https://dragonball-api.com/api/characters"; //٭ el url para las peticiones
 
-//cargarDatos sera la caja donde estara guardado los arrays, esta caja despues se usara cada q querramos manipualar lo de la pagina
-const cargarDatos = async (url) => {
+//٭ cargarDatos sera la caja donde estara guardado los arrays, esta caja despues
+//se usara cada q querramos manipualar lo de la pagina
+//٭ asyn = asincrono, funcion asincrona
+const cargarDatos = async () => {
   try {
-    const response = await fetch(url);
+    //٭ try catch= se usa por si pasa un error, si existe un error con la API
+    //muestra el error, por eso el if els, esta evaluando el valor del fetch
+    //٭ response = aca guardan las respuestas del fetch (medio obvio pero si no anoto me pierdo sola despues xd)
+    const response = await fetch();
+    //٭f etch pide los datos a la url, puede tardar
+    //٭ el await sirve para q el fetch espera a q se termine
 
+    //٭ el signo de exclamacion es la negacion del resoinse.ok, se lee "si no existe el response ok"
+    //mostra el error
     if (!response.ok) {
       throw new error("Error en la API");
     }
 
+    //٭ esta es la otra parte de la condicion, si no hay error entonces la respuesta etra en data
+    //٭ .json = es la forma q se piden los datos, la forma en la q se comunican con http
     const data = await response.json();
 
     return data;
@@ -16,4 +27,20 @@ const cargarDatos = async (url) => {
     console.log(error);
   }
 };
-x;
+
+//٭  esto es para guardar los dealles del personaje
+const verDetalles = async (id) => {
+  try {
+    const response = await fetch(`${API_URL_BASE}/${id}`);
+
+    if (!response.ok) {
+      throw new error("Error en la API");
+    }
+
+    const data = await response.json();
+
+    alert(data.description);
+  } catch (error) {
+    console.log(error);
+  }
+};
