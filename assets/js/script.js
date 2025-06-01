9434a0e34a2f6830ba859f5d8f63704fcc5bb1f8
Estudiante <luquiscardenas@gmail.com>
2025-06-01 20:09:52 -0300

    se agrego una id para el buscador, esto es para q pueda leer el valor que se ingresa en el

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,7 @@ const btnBuscar = document.getElementById("boton.buscar");
 //٭  getElementById es hace q encuentre mas facil el elemnto por el id
 const contenedorPadre = document.getElementById("contenedor-data");
 
-
-//٭ esta parte es para la funcion q carga datos en la API 
+//٭ esta parte es para la funcion q carga datos en la API
 
 //٭ cargarDatos sera la caja donde estara guardado los arrays, esta caja despues
 //se usara cada q querramos manipualar lo de la pagina
@@ -23,7 +22,9 @@ const cargarDatos = async () => {
     //٭ el signo de exclamacion es la negacion del resoinse.ok, se lee "si no existe el response ok"
     //mostra el error
     if (!response.ok) {
-      throw new error("Error en la API: ${response.status} - ${response.statusText}"); //el mensaje mostrara el mensaje y el estado guardado dentro de esas variables
+      throw new error(
+        "Error en la API: ${response.status} - ${response.statusText}"
+      ); //el mensaje mostrara el mensaje y el estado guardado dentro de esas variables
     }
 
     //٭ esta es la otra parte de la condicion, si no hay error entonces la respuesta etra en data
@@ -40,7 +41,6 @@ const cargarDatos = async () => {
   }
 };
 
-
 //esta parte  es de la funcion para ver los detalles de un pj en especifico (por la id)
 //٭  esto es para guardar los dealles del personaje
 const verDetalles = async (id) => {
@@ -51,48 +51,47 @@ const verDetalles = async (id) => {
     const response = await fetch(`${API_URL_BASE}/${id}`);
 
     if (!response.ok) {
-      throw new error("Error en la API: ${response.status} - ${response.statusText}");
+      throw new error(
+        "Error en la API: ${response.status} - ${response.statusText}"
+      );
     }
-    
 
     const data = await response.json();
 
-    //esta parte debe mostrar la descripcion del pj en un alert 
+    //esta parte debe mostrar la descripcion del pj en un alert
     alert(data.description);
   } catch (error) {
     console.error("No se cargaron los datos: ", error);
   }
 };
 
-
 //aca estaran las funciones (logica) de los pj en el DOM
 
 //٭ esta funcion es la q va a evitar el bug de las cartas duplicadas cuando buscamos un pj
 const renderizarPersonajes = (personajes) => {
-    
-    if (contenedorPadre) {
-        contenedorPadre.innerHTML = ''; 
-    } else {
-        console.error("Error: El elemento con ID 'contenedor-data' no fue encontrado en el DOM.");
-        return; // Salir de la función si no hay contenedor.
-    }
-     //٭ si el contenido de contPadre es null es pq ese id no existe en la API
-}
-
+  if (contenedorPadre) {
+    contenedorPadre.innerHTML = "";
+  } else {
+    console.error(
+      "Error: El elemento con ID 'contenedor-data' no fue encontrado en el DOM."
+    );
+    return; // Salir de la función si no hay contenedor.
+  }
+  //٭ si el contenido de contPadre es null es pq ese id no existe en la API
+};
 
 document.addEventListener("DOMContentLoaded", async () => {
-    if (contenedorPadre) {
-        contenedorPadre.innerHTML = '';
-    }
-    const data = await cargarDatos();
-    
-    console.log(dataPersonajes);
-})
+  if (contenedorPadre) {
+    contenedorPadre.innerHTML = "";
+  }
+  const data = await cargarDatos();
 
+  console.log(dataPersonajes);
+});
 
-  //٭ esta funcion sera lo q interactuara con todo el api y nos da la respuesta
+//٭ esta funcion sera lo q interactuara con todo el api y nos da la respuesta
 dataPersonajes.forEach((personaje) => {
-    contenedorPadre.innerHTML += `
+  contenedorPadre.innerHTML += `
             <div class="col-3 pb-2 d-flex justify-content-center" id="${personaje.id}">
               <div class="card" style="width: 20rem;">
                <img src=${personaje.image} class="card-img-top" alt="Imagen personaje"/>
@@ -110,10 +109,10 @@ dataPersonajes.forEach((personaje) => {
 //٭ .addEventLister es para escuchar eventos q pasan en elementos especificos
 //٭  click es el evento q estamos escuchando cuando se hace click
 btnBuscar.addEventListener("click", async (event) => {
-    event.preventDefault() //٭ este codigo es para q no recargue el formulario
+  event.preventDefault(); //٭ este codigo es para q no recargue el formulario
 
   const data = await cargarDatos();
-  if(!data) return; //si cargaDatos retorna null (pq hubo error) se sale de la funcion
+  if (!data) return; //si cargaDatos retorna null (pq hubo error) se sale de la funcion
 
   //la API devuelve los pj en un array en la propiedad de items
   const dataPersonajes = data.items;
@@ -127,33 +126,40 @@ btnBuscar.addEventListener("click", async (event) => {
 
 //٭ para no tener q ponerle un evento listener a cada boton le añadi uno solo en el contenedorPadre
 //queda mejor pq hay muchos pjs y para cuando se añadan nuevas no sea tan engorroso
-if (contenedorPadre) {//٭ esta condicion evalua el valor de verdad con la id "contenedor-data"
-    contenedorPadre.addEventListener("click", (e) => {
-        //٭ e.target' es el elemento en el q se hizo clic
-        //٭ verificamos si el elemento clicado contiene la clase 'btn-ver-detalles'
-        if (e.target.classList.contains("btn-ver-detalles")) {
-            const cardPadre = e.target.closest(".col-3");
+if (contenedorPadre) {
+  //٭ esta condicion evalua el valor de verdad con la id "contenedor-data"
+  contenedorPadre.addEventListener("click", (e) => {
+    //٭ e.target' es el elemento en el q se hizo clic
+    //٭ verificamos si el elemento clicado contiene la clase 'btn-ver-detalles'
+    if (e.target.classList.contains("btn-ver-detalles")) {
+      const cardPadre = e.target.closest(".col-3");
 
-            //٭ si se encuentra el div padre de la carta se hace esta funcion
-            //obtenemos el id del pj con su atributo data-id
-            if (cardPadre) {
-                const id = cardPadre.dataset.id;
-                //se llama a la funcion para mostrar los detalles del pj
-                verDetalles(id);
-            }
-        }
-    });
+      //٭ si se encuentra el div padre de la carta se hace esta funcion
+      //obtenemos el id del pj con su atributo data-id
+      if (cardPadre) {
+        const id = cardPadre.dataset.id;
+        //se llama a la funcion para mostrar los detalles del pj
+        verDetalles(id);
+      }
+    }
+  });
 }
 
-//٭ el addEvent es el q escucha el click 
-//٭  esta funcion 
-contenedorPadre.addEventListener("click", (e) => {
-  if (e.target.classList.contains("btn.ver-mas")) {
-    // accediendo al padre mas cercano
-    const cardPadre = e.target.closest(".col-3");
-    const id = cardPadre.dataset.id;
+//esta parte sera para la carga inicial de los personajes por default
 
-    verDetalles(id);
-  }
+//٭la parte DOMCont... se dispara cuando el html se cargue completamente
+document.addEventListener("DOMContentLoaded", async () => {
+  console.log("DOM completado");
+
+  //٭ cargamos los datos de la API
+  const data = await cargarDatos();
+  // hi hay un error al cargar no se hace nada
+  if (!data) return;
+
+  // ٭esto hara q tengamos toda la lista de los pj
+  const dataPersonajes = data.items;
+
+  console.log("Personajes cargados:", dataPersonajes);
+
+  renderizarPersonajes(dataPersonajes);
 });
-})
